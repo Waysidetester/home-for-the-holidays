f76a92e5c4a4039f1b7187cc835646ae9f3ceee7
WaysideTester <dylanmurray211@outlook.com>
2018-11-19 21:27:15 -0600

    got form elements started

---
+++ src/javascripts/components/addEditFriends/addEditFriends.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import $ from 'jquery';
+
+const formBuilder = () => {
+  const form = `
+    <div>
+      <div class="form-group">
+        <label for="form-friend-name">Name:</label>
+        <input type="text" class="form-control" id="form-friend-name" placeholder="John Smith">
+      </div>
+      <div class="form-group">
+        <label for="form-friend-email">Email:</label>
+        <input type="text" class="form-control" id="form-friend-email" placeholder="email@example.com">
+      </div>
+      <div class="form-group">
+        <label for="form-friend-phone">Phone Number:</label>
+        <input type="text" class="form-control" id="form-friend-phone" placeholder="555-555-5555">
+      </div>
+      <div class="form-group">
+        <label for="form-friend-address">Address:</label>
+        <input type="text" class="form-control" id="form-friend-address" placeholder="111 Example Way, City, ST ZIP">
+      </div>
+      <div class="form-group">
+        <label for="form-friend-relationship">relationship:</label>
+        <input type="text" class="form-control" id="form-friend-relationship" placeholder="e.g. Brother, Spouse, ...">
+      </div>
+      <div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" id="avoiding">
+        <label class="form-check-label" for="avoiding">Avoiding</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  `;
+  return form;
+};
+
+export default { formBuilder };
